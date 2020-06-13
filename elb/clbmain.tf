8d4f4075c67d2a921d375158a62c88ff4e8aa008
dd-u <54238055+dd-u@users.noreply.github.com>
2020-06-13 22:51:23 +0530

    Update clbmain.tf

--- elb/clbmain.tf
+++ elb/clbmain.tf
@@ -1,42 +1,31 @@
-resource "aws_elb" "load_balancer" {
-  name            = "${var.Name}"
-  subnets         = ["${var.Subnets}"]
-  security_groups = ["${var.Secgroup}"]
-
-  internal = false
-
-  cross_zone_load_balancing = true
-
-  connection_draining         = false
-  connection_draining_timeout = 300
-
-  idle_timeout = 60
+resource "aws_elb" "myelb" {
+  name               = "${var.Name}"
 
   listener {
     instance_port     = "${var.Instanceport}"
     instance_protocol = "${var.Instanceprotocol}"
     lb_port           = "${var.Lbport}"
     lb_protocol       = "${var.Lbprotocol}"
-    #ssl_certificate_id = listener.value.ssl_certificate_id
   }
 
   health_check {
-    #protocol            = "${var.healthcheckprotocol}"
-    #path                = "${var.healthcheckpath}"
-    target              = "TCP:80"
-    timeout             = 5
-    interval            = 30
+    healthy_threshold   = 2
     unhealthy_threshold = 2
-    healthy_threshold   = 10
+    timeout             = 3
+    target              = "HTTP:8080/"
+    interval            = 30
   }
 
+  instances                   = ["${var.Instance}"]
+  subnets                     = ["${var.Subnets}"]
+  security_groups             = ["${var.Secgroup}"]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = false
+  connection_draining_timeout = 400
+
   tags = {
-    Name                 = "${var.Name}"
-    Farm                 = "${var.Farm}"
+    Name = "${var.Farm}"
   }
-  #vpc_id            = "${var.vpc_cidr}"
-  #instance_id        = "${var.Instanceid}" 
 }
-resource "aws_vpc" "vpc" {
-  cidr_block = "${var.vpc_cidr}"
-}
+
