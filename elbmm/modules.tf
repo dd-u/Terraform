dad4d67e6d3c1b23a5dd5d3b5f8a43eb2dedc766
dd-u <54238055+dd-u@users.noreply.github.com>
2020-06-14 16:57:14 +0530

    Update modules.tf

--- elbmm/modules.tf
+++ elbmm/modules.tf
@@ -10,5 +10,5 @@ module "elb" {
   #healthcheckpath     = "hcpath"
   Instance            = "instanceid" 
   Name                = "name"
-  Env                = "Env"
+  Environment                = "Env"
  }
