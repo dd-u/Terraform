17cf8bee5d8f9ab8f6d1fb2315d871ab967f2403
dd-u <54238055+dd-u@users.noreply.github.com>
2020-06-15 14:51:35 +0530

    Update modules.tf

--- elbmm/modules.tf
+++ elbmm/modules.tf
@@ -1,13 +1,11 @@
 module "elb" {
   source              = "/var/lib/jenkins/workspace/CloudOps-Terraform/Management/ELB/elb"
-  Subnets              = "subnetid"
   Instanceport        = "insport"
   Instanceprotocol    = "insprotocol"
   Lbport              = "lbport"
   Lbprotocol          = "lbprotocol"
   #healthcheckprotocol = "hcprotocol"
   #healthcheckpath     = "hcpath"
-  Instance            = "instanceid" 
   Name                = "name"
   Environment         = "tt"
  
