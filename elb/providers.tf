f5c4ed0d1253a3de8971639e370d128029930622
dd-u <54238055+dd-u@users.noreply.github.com>
2020-06-18 08:52:07 +0530

    Update providers.tf

--- elb/providers.tf
+++ elb/providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIAZQDDAIWOHEAI3IA4"
-  secret_key = "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"
+  access_key = "AKIAZQDDAIWOHMTRQO5W"
+  secret_key = "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"
   }
