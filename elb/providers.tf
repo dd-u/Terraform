882b3d90b5423f181bd01dbc47334bfaa1a2026e
dd-u <54238055+dd-u@users.noreply.github.com>
2020-06-24 10:16:49 +0530

    Update providers.tf

--- elb/providers.tf
+++ elb/providers.tf
@@ -1,4 +1,4 @@
 resource "aws_lb_target_group_attachment" "my-attachment" {
   target_group_arn = "${aws_lb_target_group.my-tg.arn}"
-  target_id        = "${var.instance_id}"
- }
+  target_id        = 
+ 
